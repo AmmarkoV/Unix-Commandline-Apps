890f5522b1d7422dfe7be4dcf52825a41eac69f6
Ammar Qammaz <ammarkov@gmail.com>
2013-08-21 07:14:40 +0300

    ...

--- lddr/main.c
+++ lddr/main.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+#define SIZEOFOUTPUT 1024
+
+int runLdd(char * filename)
 {
-    printf("lddr !\n");
+ char output[SIZEOFOUTPUT]={0};
+
+ char * command=(char * ) malloc(sizeof(char) * (strlen(filename)+10) );
+ if (command==0) { fprintf(stderr,"Could not allocate enough memory\n"); return 0; }
+ FILE *fp=0;
+
+
+ strcpy(command,"ldd ");
+ strcat(command,filename);
+
+ // Open the command for reading.
+ fp = popen(command, "r");
+ if (fp == 0 )
+       {
+         fprintf(stderr,"Failed to run `%s`\n",filename);
+         free(command);
+         return 0;
+       }
+
+ // Read the output a line at a time - output it.
+  unsigned int i=0;
+  while (fgets(output, SIZEOFOUTPUT  , fp) != 0)
+    {
+        ++i;
+        fprintf(stderr,"Line %u , %s \n",i,output);
+    }
+
+
+  // close
+  pclose(fp);
+  free(command);
+
+  return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, const char* argv[])
+{
+    runLdd(argv[1]);
     return 0;
 }
