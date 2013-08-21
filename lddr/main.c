7e30e1e7a348097f5e396a0651b24baa8d0998fc
Ammar Qammaz <ammarkov@gmail.com>
2013-08-21 07:21:59 +0300

    ...

--- lddr/main.c
+++ lddr/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+
+
+#define NORMAL "\033[0m"
+#define BLACK "\033[30m" /* Black */
+#define RED "\033[31m" /* Red */
+#define GREEN "\033[32m" /* Green */
+#define YELLOW "\033[33m" /* Yellow */
+#define BLUE "\033[34m" /* Blue */
+#define MAGENTA "\033[35m" /* Magenta */
+#define CYAN "\033[36m" /* Cyan */
+#define WHITE "\033[37m" /* White */
+#define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
+#define BOLDRED "\033[1m\033[31m" /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m" /* Bold White */
 
 #define SIZEOFOUTPUT 1024
 
@@ -30,7 +51,14 @@ int runLdd(char * filename)
   while (fgets(output, SIZEOFOUTPUT  , fp) != 0)
     {
         ++i;
-        fprintf(stderr,"Line %u , %s \n",i,output);
+
+        if ( strstr(output,"not found")!=0 ) {
+                                               fprintf(stderr,RED "Not Found - Line %u , %s \n" NORMAL,i,output);
+                                             } else
+                                             {
+                                               fprintf(stderr,GREEN "Found - Line %u , %s \n" NORMAL,i,output);
+                                             }
+
     }
 
 
