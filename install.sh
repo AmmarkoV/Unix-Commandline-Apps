6b0fe9d7bc65583b73beb1c56443a27961595ca7
Ammar Qammaz <ammarkov@ics.forth.gr>
2017-10-20 15:44:29 +0300

    ,,,

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ echo "Copying to system bin directory"
 sudo cp pick/bin/Release/pick /bin/pick
 sudo cp sp/bin/Release/sp /bin/sp
 sudo cp lddr/lddr /bin/lddr
-sudo cp paramspy/paramSpy /bin/paramspy
+sudo cp paramspy/paramspy /bin/paramspy
 exit 0
