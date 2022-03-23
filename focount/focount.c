ba3cc5c4df2bbcf87d7e975b7edb0ce7d4b508ab
Ammar Qammaz <ammarkov@gmail.com>
2022-03-23 03:52:57 +0200

    ...

--- focount/focount.c
+++ focount/focount.c
@@ -40,11 +40,14 @@ unsigned int countOccurences(const char * bigBuffer,unsigned int bigBufferSize,c
 
       if (matched==smallPaternSize)
       {
-        ptr = smallPattern;
         matched=0;
+        ptr = smallPattern;
         occurences+=1;
        }
-
+    } else
+    {
+      matched=0;
+      ptr = smallPattern;
     }
 
     bigBuffer+=1;
@@ -58,6 +61,8 @@ unsigned int countOccurences(const char * bigBuffer,unsigned int bigBufferSize,c
 //I wrote this because of a project I am involved with
 //where I need to count occurances of a pattern in ungodly big files
 //hope someone else finds it useful.
+// It is basically a glorified  ` cat dataset.json | grep -o annotationId | wc -l `
+
 int main(int argc, const char* argv[])
 {
   if (argc<2)
@@ -66,32 +71,51 @@ int main(int argc, const char* argv[])
      return 1;
    }
 
+  const unsigned int blockSize = 32000; //32K
   //------------------------------------------
   const char * filename = argv[1];
   const char * pattern  = argv[2];
   unsigned int patternSize = strlen(pattern);
   //------------------------------------------
+  if (patternSize >= blockSize)
+  {
+      fprintf(stderr,"Pattern is too big, cannot accommodate it\n");
+      return 1;
+  }
+  //------------------------------------------
+
+
   FILE * fp = fopen(filename,"r");
   if (fp!=0)
   {
-  const unsigned int blockSize = 32000; //32K
-  char * buffer = (char *) malloc(sizeof(char) * (blockSize+1) );
-  if (buffer!=0)
-  {
-  buffer[blockSize]=0; //<- always null-terminated no matter what
+   //Our buffer is always :
+   // PATTERN    | ------------------- BUFFER -------------------------------|
+   //After each read we copy the last part of the buffer to the pattern area to
+   //count it again in case of broken patterns
+   char * buffer = (char *) malloc(sizeof(char) * (blockSize+patternSize+1) );
+   if (buffer!=0)
+   {
+    //Make sure initial pattern buffer is clear
+    memset(buffer,0,sizeof(char) * (patternSize));
+    //Make sure the buffer is
+    buffer[blockSize]=0; //<- always null-terminated no matter what
 
     unsigned long long occurences = 0;
     unsigned int thisReadLength = blockSize;
 
     while (thisReadLength==blockSize)
     {
-      thisReadLength = fread(buffer, 1, blockSize , fp);
-      occurences += countOccurences(buffer,thisReadLength,pattern,patternSize);
+      thisReadLength = fread(buffer+patternSize, 1, blockSize , fp);
+      occurences += countOccurences(buffer+patternSize-1,thisReadLength,pattern,patternSize);
+
+      memcpy(buffer,buffer+(thisReadLength-patternSize),patternSize*sizeof(char));
     }
+
+    fprintf(stdout,"%llu\n",occurences);
 
     free(buffer);
     buffer = 0;
-  }
+   }
 
     fclose(fp);
     return 0;
